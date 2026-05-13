f8c8aeb040c479793e6589cebacd9c438c722dd5
Ojekunle Damilare <ojeksdarey@gmail.com>
2026-05-13 05:34:18 +0100

    Project 1 complete - Local LLM Chat API with FastAPI and Qwen 3

---
+++ chat_api.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from pydantic import BaseModel,Field
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import json
+
+app = FastAPI()
+llm = ChatOllama(model="qwen3:14b",temperature=0)
+class QuestionInput(BaseModel):
+    question:str
+    topic:str ="general knowledge"
+
+class AnswerOutput(BaseModel):
+    question:str = Field(description="The orginal question")
+    reasoning:str =Field(description="Step by step thinking")
+    result:str = Field(description="Final concise answer")
+
+
+
+# prompt = ChatPromptTemplate([
+#     ("system","You an expert in {topic}.Reason step by step"),
+#     ("user","{question}")
+# ])
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """You are an expert in {topic}.
+Always respond with valid JSON only. No extra text.
+Format:
+{{
+  "question": "the original question",
+  "reasoning": "your step by step thinking",
+  "result": "your final concise answer"
+}}"""),
+    ("user", "{question}")
+])
+
+chain = prompt | llm | StrOutputParser()
+
+@app.post("/chat")
+
+def chat(input:QuestionInput):
+    response = chain.invoke(
+        {
+            "topic":input.topic,
+            "question":input.question
+        }
+    )
+    data = json.loads(response)
+    return AnswerOutput(**data)
+@app.get('/')
+def root():
+    return {"status":"API Agent is Running"}
